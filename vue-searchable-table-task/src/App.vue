29acefa178dc96c375d34df7174d5f43d0355e9c
Joanna Siig <joanna@siig.ee>
2020-06-10 23:19:41 +0300

    Added .gitignore, basic scaffolding for Vue task

---
+++ vue-searchable-table-task/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {}
+}
+</script>
+
+<style lang="scss">
+html, body {
+  margin: 0;
+  width: 100%;
+  height: 100%;
+
+  &, * {
+    box-sizing: border-box;
+  }
+}
+
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ vue-searchable-table-task/src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
+
+Vue.config.devtools = true // Enable devtools on production
