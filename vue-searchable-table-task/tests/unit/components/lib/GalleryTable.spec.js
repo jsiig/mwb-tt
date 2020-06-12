273a7ab6510d5a958ca249f15793f715611f7d4a
Joanna Siig <joanna@siig.ee>
2020-06-12 15:25:27 +0300

    Added basic reordering and filtering spec.

--- vue-searchable-table-task/tests/unit/components/lib/GalleryTable.spec.js
+++ vue-searchable-table-task/tests/unit/components/lib/GalleryTable.spec.js
@@ -42,4 +42,45 @@ describe('Main logic - GalleryTable component', () => {
     expect(wrapper.text()).to.include('Search my')
     expect(wrapper.text()).not.to.include('Dont find')
   })
+
+  it('reorders photos based on album ID', async () => {
+    const wrapper = mount(GalleryTable, {
+      propsData: {
+        photos: [
+          { id: 1, albumId: 1, albumTitle: 'Displayable title', title: 'A Photo To Split On', thumbnailUrl: 'https://via.placeholder.com/150/92c952' },
+          { id: 2, albumId: 2, albumTitle: 'Search my album', title: 'A Second Photo', thumbnailUrl: 'https://via.placeholder.com/150/92c952' }
+        ],
+        albums: []
+      }
+    })
+
+    const sortButton = wrapper.find('.sort-button--enabled')
+    await sortButton.trigger('click')
+
+    expect(wrapper.text().split('A Photo To Split On')[0]).to.include('Search my')
+    expect(wrapper.text().split('Displayable title')[1]).not.to.include('Search my')
+  })
+
+  it('filters by albumID', async () => {
+    const wrapper = mount(GalleryTable, {
+      propsData: {
+        photos: [
+          { id: 1, albumId: 1, albumTitle: 'Displayable title', title: 'A Photo', thumbnailUrl: 'https://via.placeholder.com/150/92c952' },
+          { id: 2, albumId: 2, albumTitle: 'Search my album', title: 'A Second Photo', thumbnailUrl: 'https://via.placeholder.com/150/92c952' }
+        ],
+        albums: []
+      }
+    })
+
+    const filters = wrapper.vm.query.albumFilters
+    filters.push({ enabled: true, id: 2 })
+    await wrapper.vm.setPhotos()
+
+    expect(wrapper.text()).to.include('Search my')
+    expect(wrapper.text()).not.to.include('Displayable title')
+
+    filters.pop()
+    await wrapper.vm.setPhotos()
+    expect(wrapper.text()).to.include('Displayable title')
+  })
 })
